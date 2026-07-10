c0f8642ac9974eb4bc561fdd0a3d3102e66b62e8
zerolong <kyl010514@naver.com>
2026-07-10 17:23:08 +0900

    Add Vercel Analytics tracking

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,23 +1,25 @@
 import Script from "next/script";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/next";
 
 const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
 export function Analytics() {
-  if (!gaId) {
-    return null;
-  }
-
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
-      <Script id="ga-script" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaId}');
-        `}
-      </Script>
+      {gaId ? (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+          <Script id="ga-script" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}');
+            `}
+          </Script>
+        </>
+      ) : null}
+      <VercelAnalytics />
     </>
   );
 }
